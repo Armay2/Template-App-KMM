9890df18ab260391a4843bc7aecea683e21f8cd5
Arnaud <arnaud.nommay@icloud.com>
2026-04-21 10:15:12 +0200

    style(shared): ktlintFormat AndroidSettingsFactory one-liner

--- shared/src/androidMain/kotlin/com/electra/template/data/storage/AndroidSettingsFactory.kt
+++ shared/src/androidMain/kotlin/com/electra/template/data/storage/AndroidSettingsFactory.kt
@@ -5,6 +5,5 @@ import com.russhwolf.settings.Settings
 import com.russhwolf.settings.SharedPreferencesSettings
 
 class AndroidSettingsFactory(private val context: Context) : SettingsFactory {
-    override fun create(name: String): Settings =
-        SharedPreferencesSettings(context.getSharedPreferences(name, Context.MODE_PRIVATE))
+    override fun create(name: String): Settings = SharedPreferencesSettings(context.getSharedPreferences(name, Context.MODE_PRIVATE))
 }
